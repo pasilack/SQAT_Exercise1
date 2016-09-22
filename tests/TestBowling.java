6b6cf26db03a465b744663663fe90b29b059a72e
somename <someemail>
2016-09-22 15:24:22 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -19,5 +19,21 @@ public class TestBowling {
 		bg.addFrame(new Frame(4, 5));
 		assertEquals(14, bg.score());
 	}
+	
+	@Test
+	public void test10FramesWithNoSparesOrStrikes() {
+		BowlingGame bg = new BowlingGame();
+		bg.addFrame(new Frame(5, 0));
+		bg.addFrame(new Frame(0, 5));
+		bg.addFrame(new Frame(0, 5));
+		bg.addFrame(new Frame(5, 0));
+		bg.addFrame(new Frame(4, 5));
+		bg.addFrame(new Frame(4, 5));
+		bg.addFrame(new Frame(4, 5));
+		bg.addFrame(new Frame(4, 0));
+		bg.addFrame(new Frame(4, 5));
+		bg.addFrame(new Frame(0, 5));
+		assertEquals(64, bg.score());
+	}
 
 }
