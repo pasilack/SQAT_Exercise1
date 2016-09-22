cbabe91d2559d69470f5587414e5f0b877cdf98b
somename <someemail>
2016-09-22 15:10:17 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 //Finish time:
 //ID:
@@ -8,6 +9,8 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
+	
+	private ListIterator<Frame> it = frames.listIterator();
 	
 	public BowlingGame(){}
 	
@@ -18,12 +21,25 @@ public class BowlingGame {
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
+		if(frames.get(0).isStrike()){
+			bonus = frames.get(1);
+		}
+			
 		//to be implemented
 	}
 	
 	// Returns the game score
 	public int score(){
-		//to be implemented: should return game score 
-		return 0;
+		int score = 0;
+		for(Frame frame: frames) {
+			if(frame.isStrike()) {
+				Frame frameplus1 = new Frame(0, 0);
+				if (it.hasNext()) {
+					  frameplus1 = it.next();
+				}
+				score += 10 + frameplus1.score();
+			}
+		}
+		return score;
 	}
 }
--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStrike() {
+		BowlingGame bg = new BowlingGame();
+		bg.addFrame(new Frame(10, 0));
+		bg.addFrame(new Frame(4, 5));
+		assertEquals(19, bg.score());
 	}
 
 }
